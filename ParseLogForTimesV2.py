e1f98046b01ef712c6c46ea81f3aabe52e7392f8
Arno Jones <Arno.Jones@IGT.com>
2019-03-08 08:53:04 -0800

    Added the bit where the outliers are found and removed from the dataset

--- ParseLogForTimesV2.py
+++ ParseLogForTimesV2.py
@@ -9,11 +9,17 @@ import re
 import pandas as pd
 from datetime import datetime
 import PySimpleGUI as sg
+import numpy as np
 
 # sample line for the following regex to match: Log Entry : 9:50:13 AM 09:50:13.902
 pattern = r"([0-9]+:[0-9]+:[0-9]+.[0-9]+)|(\d+(/|-){1}\d+(/|-){1}\d{2,4}|([0-9]+:[0-9]+:[0-9]+\s[A|P]M))"
-raw_logs_location = "\\\\USNVR-W1005006\\PublicShare\\BaccaratLogs"
-logs_results_location = '\\\\USNVR-W1005006\\PublicShare\\BaccaratLogsCSV'
+
+max_seconds = ''
+min_seconds = ''
+sum_elapsed = ''
+mean_seconds = ''
+std_deviation = ''
+cnt_samples = ''
 
 timeFormat = '%H:%M:%S.%f'
 
@@ -21,60 +27,105 @@ time_differences = []
 
 files_read = []
 
-def load_engine():
+def create_empty_folder(folder_name):
+    if not os.path.exists(folder_name):
+        print("creating", folder_name)
+        os.makedirs(folder_name)
+
+outliers = []
+def detect_outlier(data_1):
+    threshold = 3
+    mean_1= np.mean(data_1)
+    std_1 = np.std(data_1)
+
+    for y in data_1:
+        z_score = (y-mean_1)/std_1
+        if np.abs(z_score) > threshold:
+            outliers.append(y)
+    return outliers
+
+def load_engine(raw_logs_location, logs_results_location):
+
     for filename in os.listdir(raw_logs_location):
-        files_read.append(filename)
-        print(filename)
-        old = '00:00:00.000'
-        temp_list = []
-        with open(raw_logs_location + '/' + filename) as f:
-            fileContents = f.read()
-            counter = 0
-            matches = re.findall(pattern, fileContents, re.MULTILINE)
+            files_read.append(filename)
+            print(filename)
+            old = '00:00:00.000'
+            temp_list = []
+            with open(raw_logs_location + '/' + filename) as f:
+                fileContents = f.read()
+                counter = 0
+                matches = re.findall(pattern, fileContents, re.MULTILINE)
 
-            for matchNum, match in enumerate(matches):
-                log_entry_date = matches[counter + 0][1]
-                log_entry_time = matches[counter + 1][1]
-                game_event_timestamp = matches[counter + 2][0]
+                for matchNum, match in enumerate(matches):
+                    log_entry_date = matches[counter + 0][1]
+                    log_entry_time = matches[counter + 1][1]
+                    game_event_timestamp = matches[counter + 2][0]
 
-                new = game_event_timestamp
-                deltaT = datetime.strptime(new, timeFormat) - datetime.strptime(old, timeFormat)
+                    new = game_event_timestamp
+                    deltaT = datetime.strptime(new, timeFormat) - datetime.strptime(old, timeFormat)
 
-                deltaTinSeconds = deltaT.total_seconds()
+                    deltaTinSeconds = deltaT.total_seconds()
 
-                old = new
-                temp_list.append([filename, log_entry_date, log_entry_time, (deltaTinSeconds)])
+                    old = new
+                    temp_list.append([filename, log_entry_date, log_entry_time, deltaTinSeconds])
 
-                if matchNum < len(matches)/3 -1: # 3 elements per match -1 for index out of range
-                    counter += 3
-                else:
-                    for i in range(2):
-                        temp_list.pop(0) # delete the entry that subtracts current time from 0, as that's meaningless.
-                    break
-            time_differences.append(temp_list)
+                    if matchNum < len(matches) / 3 - 1:  # 3 elements per match -1 for index out of range
+                        counter += 3
+                    else:
+                        for i in range(2):
+                            try:
+                                temp_list.pop(0)  # delete the entry that subtracts current time from 0, as that's meaningless.
+                            except IndexError:
+                                print("Not enough game states to calculate (requires at least 2 complete game cycles.)")
+                        break
+                time_differences.append(temp_list)
 
-    df = pd.DataFrame(item for sublist in time_differences for item in sublist) # convert list of lists into list
+    df = pd.DataFrame(item for sublist in time_differences for item in sublist)  # convert list of lists into dataframe
 
-    df.columns = ['Client EGM','Log Entry Date','Log Entry Time','Elapsed Time']
+    df.columns = ['Client EGM', 'Log Entry Date', 'Log Entry Time', 'Elapsed Time']
 
-    print('MAX seconds', df['Elapsed Time'].max())
-    print('MIN seconds', df['Elapsed Time'].min())
-    print('SUM of all seconds', df['Elapsed Time'].sum())
-    print('MEAN time of the delta Ts', df['Elapsed Time'].mean())
-    print('STANDARD DEVIATION ', df['Elapsed Time'].std())
-    print('COUNT of samples', df['Elapsed Time'].count())
+    # find and print the outliers
+    outlier_datapoints = detect_outlier(df['Elapsed Time'])
+    print(outlier_datapoints)
+
+    # find the outliers and remove them from the dataset
+    df2 = df[np.abs(df['Elapsed Time'] - df['Elapsed Time'].mean()) <= (3 * df['Elapsed Time'].std())]
+
+    global max_seconds
+    global min_seconds
+    global sum_elapsed
+    global mean_seconds
+    global std_deviation
+    global cnt_samples
+
+    max_seconds = df2['Elapsed Time'].max()
+    min_seconds = df2['Elapsed Time'].min()
+    sum_elapsed = df2['Elapsed Time'].sum()
+    mean_seconds = df2['Elapsed Time'].mean()
+    std_deviation = df2['Elapsed Time'].std()
+    cnt_samples = df2['Elapsed Time'].count()
+
+    print('MAX seconds', df2['Elapsed Time'].max())
+    print('MIN seconds', df2['Elapsed Time'].min())
+    print('SUM of all seconds', df2['Elapsed Time'].sum())
+    print('MEAN time of the delta Ts', df2['Elapsed Time'].mean())
+    print('STANDARD DEVIATION ', df2['Elapsed Time'].std())
+    print('COUNT of samples', df2['Elapsed Time'].count())
 
     current_time = str(datetime.now().date())
 
     # --- write to summary file --- #
+    # see if dir exists.  if not, create it.
+    create_empty_folder(logs_results_location)
+
     with open(logs_results_location + '\\' + current_time + '_BaccaratStats.txt', 'w+') as f:
         f.writelines(str(len(files_read)) + ' log files read.' + '\r\n\r\n')
-        f.write('MAX seconds: ' + str(df['Elapsed Time'].max()) + '\r\n')
-        f.write('MIN seconds: ' + str(df['Elapsed Time'].min()) + '\r\n')
-        f.write('SUM of all elapsed times: ' + str(df['Elapsed Time'].sum()) + '\r\n')
-        f.write('MEAN seconds: ' + str(df['Elapsed Time'].mean()) + '\r\n')
-        f.write('STANDARD DEVIATION: ' + str(df['Elapsed Time'].std()) + '\r\n')
-        f.write('COUNT of all samples: ' + str(df['Elapsed Time'].count()) + '\r\n')
+        f.write('MAX seconds: ' + str(df2['Elapsed Time'].max()) + '\r\n')
+        f.write('MIN seconds: ' + str(df2['Elapsed Time'].min()) + '\r\n')
+        f.write('SUM of all elapsed times: ' + str(df2['Elapsed Time'].sum()) + '\r\n')
+        f.write('MEAN seconds: ' + str(df2['Elapsed Time'].mean()) + '\r\n')
+        f.write('STANDARD DEVIATION: ' + str(df2['Elapsed Time'].std()) + '\r\n')
+        f.write('COUNT of all samples: ' + str(df2['Elapsed Time'].count()) + '\r\n')
         f.write('\r\n')
         for file in files_read:
             f.write('Log file read: ' + file + '\r\n')
@@ -83,25 +134,22 @@ def load_engine():
     if not os.path.exists(logs_results_location):
         os.makedirs(logs_results_location)
 
-    df.to_csv(logs_results_location + '\\' + current_time + '_LogsReport.csv')
+    df2.to_csv(logs_results_location + '\\' + current_time + '_LogsReport.csv')
+
 
 # https://pysimplegui.readthedocs.io/#how-do-i
 layout = [
-    [sg.Text('Launch Baccarat Client Simulator', size = (30, 1), font=("Helvetica", 25), text_color='blue')],
-    [sg.Text('IP Address of target Baccarat server:', size=(35, 1)), sg.InputText('10.213.133.65', key='_IP_')],
-    [sg.Text('Port address of Baccarat server:', size=(35, 1)), sg.InputText('4456', key='_PORT_')],
-    [sg.Text('Logs destination, full path:', size=(35, 1)), sg.InputText(r'\\USNVR-W1005006\PublicShare\BaccaratLogs', key='_LOGSDEST')],
-    [sg.Text('Statistics and csv file destination:', size=(35, 1)), sg.InputText(r'\\USNVR-W1005006\PublicShare\BaccaratLogsCSV', key='CSVDEST')],
-    [sg.Text('How many processes to launch: ', size=(35, 1)), sg.InputText('4', key='_NUMOFPROCS_')],
-    [sg.Text('Statistics summary: ', size=(35, 1)), sg.Multiline(default_text='29 log files read\n'
-                                                         '\n'
-                                                         'MAX seconds: 108.177\n'
-                                                         'MIN seconds: 0.004\n'
-                                                         'SUM of all elapsed times: 32905.762\n'
-                                                         'MEAN seconds: 4.005570541692027\n'
-                                                         'STANDARD DEVIATION: 3.40685478981833\n'
-                                                         'COUNT of all samples: 8215',key='_SUMMARY_', size=(55, 5))],
-    [sg.Button("Create Stats File"), sg.Submit(), sg.Exit()]
+    [sg.Text('Generate Statistics From Baccarat Client Simulator', size=(45, 1), font=("Helvetica", 20),
+             text_color='blue')],
+    # [sg.Text('IP Address of target Baccarat server:', size=(35, 1)), sg.InputText('10.213.133.65', key='_IP_', do_not_clear=True)],
+    # [sg.Text('Port address of Baccarat server:', size=(35, 1)), sg.InputText('4456', key='_PORT_', do_not_clear=True)],
+    [sg.Text('Logs location, full path:', size=(40, 1)),
+     sg.InputText(r'\\USNVR-W1005006\PublicShare\BaccaratLogs', key='_LOGSDEST', do_not_clear=True)],
+    [sg.Text('Statistics and csv file destination:', size=(40, 1)),
+     sg.InputText(r'\\USNVR-W1005006\PublicShare\BaccaratLogsCSV', key='_CSVDEST_', do_not_clear=True)],
+    # [sg.Text('How many processes to launch: ', size=(35, 1)), sg.InputText('4', key='_NUMOFPROCS_', do_not_clear=True)],
+    [sg.Text('Statistics summary: ', size=(35, 1)), sg.Multiline(key='_SUMMARY_', size=(55, 6))],
+    [sg.Submit('Create Stats File'), sg.Exit()]
 ]
 
 window = sg.Window('Baccarat Client Load Test Automation').Layout(layout)
@@ -112,15 +160,26 @@ while True:
     if event is None or event == 'Exit':
         break
 
-    if event == 'Submit':
+    if event == 'Create Stats File':
         # do the stuff here
-        load_engine()
-        print('I am here.')
+        load_engine(values['_LOGSDEST'], values['_CSVDEST_'])
+        window.FindElement('_SUMMARY_').Update(
+            'MAX (seconds): {}\n'
+            'MIN (seconds): {}\n'
+            'TOTAL client-seconds: {}\n'
+            'MEAN (seconds): {}\n'
+            'STANDARD DEVIATION: {}\n'
+            'SAMPLE COUNT: {}'.format(max_seconds,
+                                      min_seconds,
+                                      sum_elapsed,
+                                      mean_seconds,
+                                      std_deviation,
+                                      cnt_samples))
 
 window.Close()
-print(values['_IP_'])
-print(values['_PORT_'])
+# print(values['_IP_'])
+# print(values['_PORT_'])
 print(values['_LOGSDEST'])
-print(values['CSVDEST'])
-print(values['_NUMOFPROCS_'])
+print(values['_CSVDEST_'])
+# print(values['_NUMOFPROCS_'])
 print(values['_SUMMARY_'])
